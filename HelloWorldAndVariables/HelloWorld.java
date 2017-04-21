4b44e77f19aa0a056dc9f95aeaec63c58351ab4d
Sandra Hawkins <sandra.hawkins@ftc.cdetb.ie>
2017-04-21 11:29:18 +0100

    Add file

---
+++ HelloWorldAndVariables/HelloWorld.java
@@ -0,0 +1,13 @@
+/*  
+ * All java classes have opening and closing brackets.
+ * All code inside a java class MUST go inside the
+ * the classes curly brackets.
+ *
+ */
+public class HelloWorld {
+	
+	public static void main(String [] args) {
+		
+		System.out.println("Hello World");
+	}
+}
