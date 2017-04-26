2e885bad97b466148c79804a13fc78b697cf1380
Sandra Hawkins <sandra.hawkins@ftc.cdetb.ie>
2017-04-26 13:52:58 +0100

    Change void method to return

--- ExerciseSolutions/Sheet2Methods/Ex8SalaryWithReturns.java
+++ ExerciseSolutions/Sheet2Methods/Ex8SalaryWithReturns.java
@@ -1,5 +1,3 @@
-/*******  STILL to finish ******/
-
 public class Ex8SalaryWithReturns {
 
 	public static void main(String [] arg) { 
@@ -9,23 +7,58 @@ public class Ex8SalaryWithReturns {
 		Ex8SalaryWithReturns ex8 = new Ex8SalaryWithReturns();
 		
 		double salary = 30_000;
-		ex5.printMonthlySalary(salary);
-		ex5.salaryAfterPayRise(salary, 4);
-		ex5.salaryAfterPayRise(salary, 6);
+		double monthly = ex8.printMonthlySalary(salary);
+		System.out.printf("Monthly salary %.2f\n", monthly);
+		//double weekly = ex8.printWeeklySalary(salary);
+		
+		/* You can call a method within the println or printf, the 
+		method must return a value, i.e. you cannot call a void method
+		from println or printf*/
+		System.out.printf("Weekly salary %.2f\n", 
+						  ex8.printWeeklySalary(salary));
+						  
+		double newSalary = ex8.salaryAfterPayRise(salary, 4);
+		System.out.printf("New salary after 4%% pay rise %.2f\n", 
+						   newSalary);
+		newSalary = ex8.salaryAfterPayRise(salary, 6);
+		System.out.printf("New salary after 6%% pay rise %.2f\n", 
+						   newSalary);
+				
+		/* The following line of code will not compile, you cannot
+		call a void method (i.e. a method that returns nothing) from
+		within a call to println() or printf() */		
+		//System.out.println("Calling printHello() " + ex8.printHello());
+	}
+	
+	public void printHello() {
+		System.out.println("Hello");
 	}
 	
 	/*
 		salary is the yearly salary.
 		payRise is the percentage pay rise, e.g. 4%.
-	*/					//                30000			4
-	public void salaryAfterPayRise(double salary, double payRise) {
+	*/					
+	public double salaryAfterPayRise(double salary, double payRise) {
 		double result = salary + (salary * (payRise / 100));
-		System.out.printf("Salary after pay rise %.2f\n", result);	
+		//System.out.printf("Salary after pay rise %.2f\n", result);	
+		return result;
 	}
-			//								30000					
-	public void printMonthlySalary(double yearlySalary) {
-		double salary = yearlySalary / 12;   // 2500
-		
-		System.out.printf("Monthly salary is : %.2f\n", salary);
+								
+	public double printMonthlySalary(double yearlySalary) {
+		double salary = yearlySalary / 12;   
+		// System.out.printf("Monthly salary is : %.2f\n", salary);
+		return salary;
+	}
+	
+	public double printWeeklySalary(double yearlySalary) {
+		double salary = yearlySalary / 52;   
+		// System.out.printf("Weekly salary is : %.2f\n", salary);
+		return salary;
 	}
 }
+/*** OUTPUT
+Monthly salary 2500.00
+Weekly salary 576.92
+New salary after 4% pay rise 31200.00
+New salary after 6% pay rise 31800.00
+*/
