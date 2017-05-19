c93bf604b40d16293eaf12d5d4aa7655ebcaa844
SandraHawkins <SandraHawkins@users.noreply.github.com>
2017-05-19 10:50:49 +0100

    Remove comments

--- ExerciseSolutions/src/sheet5Classes/MainForEmail.java
+++ ExerciseSolutions/src/sheet5Classes/MainForEmail.java
@@ -13,11 +13,11 @@ public class MainForEmail {
 				"hello@there.ie", "A12a34", EmailAccount.STORAGE_500_GB);
 		System.out.println(account1);
 		
-//		EmailAccount account2 = new EmailAccount("sandra.hawkins@test.com", 
-//				"123456", EmailAccount.STORAGE_1_TB);
-//		System.out.println(account2);
-//		
-//		EmailAccount account3 = new EmailAccount("me@me.com", "abcdef", 10000);
-//		System.out.println(account3);
+		EmailAccount account2 = new EmailAccount("sandra.hawkins@test.com", 
+				"123456", EmailAccount.STORAGE_1_TB);
+		System.out.println(account2);
+		
+		EmailAccount account3 = new EmailAccount("me@me.com", "abcdef", 10000);
+		System.out.println(account3);
 	}
 }
